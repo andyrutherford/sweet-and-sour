81064d314f17c7bb40ff21483d091cf99ef8b2f1
Andy <andrew.rutherford.6@gmail.com>
2020-10-06 11:00:49 +0200

    establish connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import products from './data/products.js';
 
-const products = require('./data/products');
-
+dotenv.config();
+connectDB();
 const app = express();
 
 app.get('/api/products', (req, res) => {
@@ -12,4 +15,11 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log(`Server running on port 5000`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`
+  )
+);
