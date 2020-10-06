4e138e37b39f8fcbfa8679678bf6344518626819
Andy <andrew.rutherford.6@gmail.com>
2020-10-06 11:20:45 +0200

    Order Product and User Schemas

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import connectDB from './config/db.js';
 import products from './data/products.js';
 
@@ -21,5 +22,6 @@ app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`
+      .yellow.bold
   )
 );
