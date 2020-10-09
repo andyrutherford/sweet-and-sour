5bce2bae81095a60d7e94eb869f7e3b4f8c0392f
Andy <andrew.rutherford.6@gmail.com>
2020-10-09 09:23:44 +0200

    updateUserProfile route and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,4 +74,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, createUser };
+// @desc        Update user profile
+// @route       PUT /api/users/profile
+// @access      PRIVATE
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      // Password will be encrypted before user.save() because of middleware in schema
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found.');
+  }
+});
+
+export { authUser, getUserProfile, createUser, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,7 @@ import {
   authUser,
   getUserProfile,
   createUser,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
@@ -12,5 +13,6 @@ const router = express.Router();
 router.route('/').post(createUser);
 router.route('/login').post(authUser);
 router.route('/profile').get(protect, getUserProfile);
+router.route('/profile').put(protect, updateUserProfile);
 
 export default router;
